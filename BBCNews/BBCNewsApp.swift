d9dc4d97b16880f70d6f4d82f42c2c6b6864538a
Ana Stefan <ana.stefan1000@gmail.com>
2023-10-25 22:53:07 +0300

    Initial Commit

---
+++ BBCNews/BBCNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBCNewsApp.swift
+//  BBCNews
+//
+//  Created by Ana Ștefan on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BBCNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
