015c623a46281a41e7466651875460df1e548d07
Ana Stefan <ana.stefan1000@gmail.com>
2023-10-26 00:12:54 +0300

    added LaunchScreen added appIcon added app logo

--- BBCNews/ContentView.swift
+++ BBCNews/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, ana")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            Text("Content View")
+                .foregroundColor(.white)
+                .font(.system(size: 18))
         }
-        .padding()
     }
 }
 
