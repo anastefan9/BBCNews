3998a9f65c5d01a18ff606dec9bc39cbe983b654
Ana Stefan <ana.stefan1000@gmail.com>
2023-10-25 22:58:15 +0300

    changed text

--- BBCNews/ContentView.swift
+++ BBCNews/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, xxx")
+            Text("Hello, ana")
         }
         .padding()
     }
